3bb2a62af8e3520e4f8ef27f37eb930b6ce3d9e3
John Soza <johnsoza1988@gmail.com>
2026-03-12 15:26:53 -0700

    additional minor fix

--- scenes_and_functions.py
+++ scenes_and_functions.py
@@ -26,7 +26,7 @@ def enter_name():
 
         if user_name.isspace() or user_name == "":
             print("\nPlease " + italic("ACTUALLY ENTER") + " a name.\n")
-            
+
         elif user_name.isdigit():
             print("\nNumbers are " + italic("NOT") + " names.  If there is a number in your formal name, please \nuse " + italic("Roman Numerals") + " such as " + italic("'I', 'II', 'III'") + ", etc.\n")
 
@@ -36,7 +36,7 @@ def enter_name():
             print("And " + italic("don't worry too much about failing") + "!  If you \nmake a mistake you have the option to retry from \n" + italic("the last checkpoint") + ".\n")
             print("Godspeed!\n")
             input("Press enter to continue...")
-            print(f"Valid name is {valid_name}")
+
             valid_name = True
 
     return user_name
